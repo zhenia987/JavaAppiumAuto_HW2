f431cef6755f4cdb13fd5c4b3a8aff91a3b12d94
zhenia987 <zhenia_987@mail.ru>
2024-06-14 17:05:38 +0300

    n

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class FirstTest {
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("appium:devicesName","AndroidTestDevice");
+        capabilities.setCapability("appium:platformVersion","12");
+        capabilities.setCapability("appium:automationName","UiAutomator2");
+        capabilities.setCapability("appium:appPackage","org.wikipedia");
+        capabilities.setCapability("appium:appActivity",".main.MainActivity");
+        capabilities.setCapability("appium:app","/Users/mbpro/Desktop/JavAppiumAutomation/JavaAppiumAuto/JavaAppiumAuto/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http:127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest()
+    {
+        System.out.println("First test run");
+    }
 }
