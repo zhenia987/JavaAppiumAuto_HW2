723e87f1d7d5e819570f140f543486c71d528e17
zhenia987 <zhenia_987@mail.ru>
2024-06-14 17:10:41 +0300

    n231

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -25,7 +25,7 @@ public class FirstTest {
         capabilities.setCapability("appium:appActivity",".main.MainActivity");
         capabilities.setCapability("appium:app","/Users/mbpro/Desktop/JavAppiumAutomation/JavaAppiumAuto/JavaAppiumAuto/apks/org.wikipedia.apk");
 
-        driver = new AndroidDriver(new URL("http:127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http:127.0.0.1:4723/"), capabilities);
     }
 
 
