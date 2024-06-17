d071baa4943d934c38dd88d96337a631a6d99521
zhenia987 <zhenia_987@mail.ru>
2024-06-17 13:00:26 +0300

    n231

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -4,13 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.net.URL;
 
 public class FirstTest {
 
     private AppiumDriver driver;
-
 
     @Before
     public void setUp() throws Exception
@@ -18,16 +16,15 @@ public class FirstTest {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("appium:devicesName","AndroidTestDevice");
-        capabilities.setCapability("appium:platformVersion","12");
-        capabilities.setCapability("appium:automationName","UiAutomator2");
-        capabilities.setCapability("appium:appPackage","org.wikipedia");
-        capabilities.setCapability("appium:appActivity",".main.MainActivity");
-        capabilities.setCapability("appium:app","/Users/mbpro/Desktop/JavAppiumAutomation/JavaAppiumAuto/JavaAppiumAuto/apks/org.wikipedia.apk");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","12");
+        capabilities.setCapability("automationName","UiAutomator2");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/mbpro/Desktop/JavAppiumAutomation/JavaAppiumAuto/JavaAppiumAuto/apks/org.wikipedia.apk");
 
-        driver = new AndroidDriver(new URL("http:127.0.0.1:4723/"), capabilities);
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub/"), capabilities);
     }
-
 
     @After
     public void tearDown()
